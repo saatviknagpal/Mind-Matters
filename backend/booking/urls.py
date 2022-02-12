4e0233eab1c90bc5f5cfa18dc1cd75e348ddc0b6
kunal <kunalbhatia868@gmail.com>
2022-02-12 21:13:36 +0530

    Added BookSlot endpoint and users's booked slot endpoint

--- backend/booking/urls.py
+++ backend/booking/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import ProfessionalList,ProfessionalDetail,SlotList
-
+from .views import (
+    BookedSlotCreate,
+    BookedSlotList,
+    ProfessionalList,
+    ProfessionalDetail,
+    SlotList,
+    
+)
 urlpatterns=[
     path("professionals/",ProfessionalList.as_view(),name='professional-list'),
     path("professionals/<int:pk>",ProfessionalDetail.as_view(),name='professional-detail'),
     path("slots/",SlotList.as_view(),name='slot-list'),
+    path("booked-slots/",BookedSlotList.as_view(),name="booked-slots-list"),
+    path("book-slot/",BookedSlotCreate.as_view(),name="book-slot")
 ]
