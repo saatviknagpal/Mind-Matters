f363a77a088f3f47ded46dd9880c2acbb4a1f7b8
Rajib Mondal <mondal021002rajib@gmail.com>
2022-02-13 01:18:05 +0530

    Book Appointment Functionality Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import BlogsPage from "./pages/BlogsPage";
 import ProfessionalList from "./pages/ProfessionalsPage/ProfessionalList";
-import BookAppointment from "./pages/BookAppointment";
+import BookAppointment from "./pages/BookAppointment/Index";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/blogs" element={<BlogsPage />} />
         <Route exact path="/appointment" element={<ProfessionalList />} />
-        <Route exact path="/bookAppointment" element={<BookAppointment />} />
+        <Route exact path="/bookAppointment/:id" element={<BookAppointment />} />
       </Routes>
     </BrowserRouter>
   );
