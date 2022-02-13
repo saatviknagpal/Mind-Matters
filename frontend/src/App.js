c584cd2e1fab821e0c19ead80f1a395d3113ce67
Rajib Mondal <mondal021002rajib@gmail.com>
2022-02-13 13:57:11 +0530

    Jokes Page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage/Homepage";
 import BlogsPage from "./pages/BlogsPage";
 import ProfessionalList from "./pages/ProfessionalsPage/ProfessionalList";
 import BookAppointment from "./pages/BookAppointment/index";
+import JokesPage from "./pages/JokesPage/index";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Homepage />} />
         <Route exact path="/blogs" element={<BlogsPage />} />
+        <Route exact path="/jokes" element={<JokesPage />} />
         <Route exact path="/appointment" element={<ProfessionalList />} />
         <Route exact path="/bookAppointment/:id" element={<BookAppointment />} />
       </Routes>
